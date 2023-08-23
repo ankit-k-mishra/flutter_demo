c33bfddefb46038b0ecb4dbe64aa8195902f8c3c
Ankit Kumar Mishra <ankit.mishra@tatadigital.com>
2023-08-23 14:26:25 +0530

    day4

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,13 +18,42 @@ class LoginPage extends StatelessWidget {
           ),
           Text("Welcome",
           style: TextStyle(
-            fontSize: 25,
+            fontSize: 30,
             fontWeight: FontWeight.bold,
           ),
           ),
           SizedBox(
             height: 20.0,
             //child: Text("iudsfgisbofv"),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0 , horizontal:32.0 ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password"
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(),
+                  onPressed: () {
+                    print("Hi ankit welcome!!!");
+                  },
+                ),
+            ],),
           ),
         ],
       )
